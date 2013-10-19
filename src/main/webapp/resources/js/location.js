c74a758a695e3649f712713259a1f5f0ef250144
Robin Wyss <rowyss@gmail.com>
2013-10-19 21:13:29 +0200

    removed unused javascript, added licence file

--- src/main/webapp/resources/js/location.js
+++ src/main/webapp/resources/js/location.js
@@ -1,24 +1,17 @@
-function getLocation()
-{
-    if (navigator.geolocation)
-    {
-        var options = {timeout:(5 * 1000), maximumAge:(1000 * 60 * 15), enableHighAccuracy:false};
+function getLocation() {
+    if (navigator.geolocation) {
+        var options = {timeout: (5 * 1000), maximumAge: (1000 * 60 * 15), enableHighAccuracy: false};
         navigator.geolocation.getCurrentPosition(locationFound, locationUnavailable, options);
     }
 }
-function locationFound(position)
-{
+function locationFound(position) {
     var longitude = position.coords.longitude;
     var latitude = position.coords.latitude;
     $.ajax({
-        url:'/weather/search/' + latitude + '/' + longitude + '/',
-        success:function (data)
-        {
-            $('#nowcast').fadeIn('slow', function ()
-            {
-                $('.loadMessage').fadeOut('fast', function ()
-                {
-                });
+        url: '/weather/search/' + latitude + '/' + longitude + '/',
+        success: function (data) {
+            $('.loadMessage').fadeOut('fast', function () {
+                $('#nowcast').fadeIn('slow');
             });
             $("#location").append(data.name);
             $("#temperature").append(data.main.temp);
@@ -28,17 +21,6 @@ function locationFound(position)
     });
 
 }
-function locationUnavailable(error)
-{
+function locationUnavailable(error) {
     alert("could no get location " + error);
-}
-
-function enterEditMode(element)
-{
-    element.class = 'edit';
-}
-
-function leaveEditMode(element)
-{
-    element.class = 'display';
 }
